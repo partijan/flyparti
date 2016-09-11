12a6b86c45b9108c4d4d70696882a9b943c6fcaf
Jan Borzensky <jan.borzensky@e-invent.eu>
2016-09-11 22:25:59 +0200

    no message

--- test/tamplates/admin/pridatDestinaci/testingForm.php
+++ test/tamplates/admin/pridatDestinaci/testingForm.php
@@ -1,6 +1,10 @@
 <?php
 
-class testingForm extends SebastianBergmann\PHPUnit
+namespace SebastianBergmann\Money;
+
+use PHPUnit\Framework\TestCase;
+
+class testingForm extends \TestCase
 {
     
 }

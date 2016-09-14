69717254b8bdff6a9b1926b81fa8a502cd7ac8b6
Jan Borzensky <jan.borzensky@e-invent.eu>
2016-09-14 21:55:20 +0200

    no message

--- test/tamplates/admin/pridatDestinaci/testingForm.php
+++ test/tamplates/admin/pridatDestinaci/testingForm.php
@@ -6,6 +6,10 @@ use PHPUnit\Framework\TestCase;
 
 class testingForm extends \TestCase
 {
-    
+    public function firsTest(){
+        $name = new Name("Honza");
+        $result = $name->name();
+        $this->assertEquals("Honza",$result);
+    }
 }
 
